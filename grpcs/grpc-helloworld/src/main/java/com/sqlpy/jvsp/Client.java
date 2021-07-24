8644de4cc035fbda1aaa1893f67e91bca1b5b448
Neeky <1721900707@qq.com>
2021-07-24 16:25:50 +0800

    docs: 添加 grpc-helloworld 的实现

---
+++ grpcs/grpc-helloworld/src/main/java/com/sqlpy/jvsp/Client.java
@@ -0,0 +1,59 @@
+package com.sqlpy.jvsp;
+
+import com.sqlpy.jvsp.pb.GreeterGrpc;
+import com.sqlpy.jvsp.pb.HelloReply;
+import com.sqlpy.jvsp.pb.HelloRequest;
+import io.grpc.Channel;
+import io.grpc.ManagedChannel;
+import io.grpc.ManagedChannelBuilder;
+import io.grpc.StatusRuntimeException;
+import java.util.concurrent.TimeUnit;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class Client {
+    private static final Logger logger = Logger.getLogger(Client.class.getName());
+
+    private final GreeterGrpc.GreeterBlockingStub blockingStub;
+
+    public Client(Channel channel) {
+        blockingStub = GreeterGrpc.newBlockingStub(channel);
+    }
+
+    /** Say hello to server. */
+    public void greet(String name) {
+        logger.info("Will try to greet " + name + " ...");
+        HelloRequest request = HelloRequest.newBuilder().setName(name).build();
+        HelloReply response;
+        try {
+            response = blockingStub.sayHello(request);
+        } catch (StatusRuntimeException e) {
+            logger.log(Level.WARNING, "RPC failed: {0}", e.getStatus());
+            return;
+        }
+        logger.info("Greeting: " + response.getMessage());
+    }
+
+    public static void main(String[] args) throws Exception {
+        String user = "world";
+        // Access a service running on the local machine on port 50051
+        String target = "localhost:50051";
+        // Create a communication channel to the server, known as a Channel. Channels are thread-safe
+        // and reusable. It is common to create channels at the beginning of your application and reuse
+        // them until the application shuts down.
+        ManagedChannel channel = ManagedChannelBuilder.forTarget(target)
+                // Channels are secure by default (via SSL/TLS). For the example we disable TLS to avoid
+                // needing certificates.
+                .usePlaintext()
+                .build();
+        try {
+            Client client = new Client(channel);
+            client.greet(user);
+        } finally {
+            // ManagedChannels use resources like threads and TCP connections. To prevent leaking these
+            // resources the channel should be shut down when it will no longer be used. If it may be used
+            // again leave it running.
+            channel.shutdownNow().awaitTermination(5, TimeUnit.SECONDS);
+        }
+    }
+}
