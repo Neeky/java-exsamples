ecf2fbdd24fb1f8f4822ad5eb7f70a0cd63ac3e9
Neeky <1721900707@qq.com>
2021-07-24 15:23:39 +0800

    feat: 添加 grpc 相关的示例项目

---
+++ grpcs/grpc-exsameple/src/main/java/com/sqlpy/grpcexsample/App.java
@@ -0,0 +1,10 @@
+package com.sqlpy.grpcexsample;
+
+import com.sqlpy.grpcexsample.pb.Person;
+
+public class App {
+    public static void main(String[] args) {
+        System.out.println("hello grpc exsamples.");
+        System.out.println(Person.class);
+    }
+}
